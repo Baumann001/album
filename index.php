bfede9e6ae496638b150ad0909414a4719af7c53
Baumann001 <gustavo_baumann@estudante.sesisenai.org.br>
2025-10-22 08:33:22 -0300

    add-

--- index.php
+++ index.php
@@ -16,7 +16,7 @@ $result = $conn->query($sql);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Privacy - Upload de Fotos</title>
+    <title>Álbum - Upload de Fotos</title>
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
